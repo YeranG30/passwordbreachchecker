ccf6cfcc057ae28d923daeac0c758b6337ebca70
YeranG30 <74067706+YeranG30@users.noreply.github.com>
2023-09-03 16:32:11 -0400

    Update pwdchecker.py
    added interface, banner, and slight output changes

--- pwdchecker.py
+++ pwdchecker.py
@@ -1,36 +1,57 @@
 import time
 
+# Superhero ASCII Art
+ART = """
+██▓███   ▄▄▄       ██████   ██████  █     █░ ▒█████   ██▀███  ▓█████▄      ▄████▄   ██░ ██  ▓█████  ▄████▄  ██ ▄█▀ ▓█████ ██▀███      
+▓██░  ██ ▒████▄   ▒██    ▒ ▒██    ▒ ▓█░ █ ░█░▒██▒  ██▒▓██ ▒ ██▒▒██▀ ██▌    ▒██▀ ▀█ ▒▓██░ ██  ▓█   ▀ ▒██▀ ▀█  ██▄█▒  ▓█   ▀▓██ ▒ ██▒    
+▓██░ ██▓▒▒██  ▀█▄ ░ ▓██▄   ░ ▓██▄   ▒█░ █ ░█ ▒██░  ██▒▓██ ░▄█ ▒░██   █▌    ▒▓█    ▄░▒██▀▀██  ▒███   ▒▓█    ▄▓███▄░  ▒███  ▓██ ░▄█ ▒    
+▒██▄█▓▒ ▒░██▄▄▄▄██  ▒   ██▒  ▒   ██▒░█░ █ ░█ ▒██   ██░▒██▀▀█▄  ░▓█▄   ▌    ▒▓▓▄ ▄██ ░▓█ ░██  ▒▓█  ▄▒▒▓▓▄ ▄██▓██ █▄  ▒▓█  ▄▒██▀▀█▄      
+▒██▒ ░  ░▒▓█   ▓██▒██████▒▒▒██████▒▒░░██▒██▓ ░ ████▓▒░░██▓ ▒██▒░▒████▓     ▒ ▓███▀  ░▓█▒░██▓▒░▒████░▒ ▓███▀ ▒██▒ █▄▒░▒████░██▓ ▒██▒    
+▒▓▒░ ░  ░░▒▒   ▓▒█▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░ ▓░▒ ▒  ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒▓  ▒     ░ ░▒ ▒    ▒ ░░▒░▒░░░ ▒░ ░░ ░▒ ▒  ▒ ▒▒ ▓▒░░░ ▒░ ░ ▒▓ ░▒▓░    
+░▒ ░     ░ ░   ▒▒ ░ ░▒  ░ ░░ ░▒  ░ ░  ▒ ░ ░    ░ ▒ ▒░   ░▒ ░ ▒  ░ ▒  ▒       ░  ▒    ▒ ░▒░ ░░ ░ ░     ░  ▒  ░ ░▒ ▒░░ ░ ░    ░▒ ░ ▒     
+░░         ░   ▒  ░  ░  ░  ░  ░  ░    ░   ░  ░ ░ ░ ▒    ░░   ░  ░ ░  ░     ░         ░  ░░ ░    ░   ░       ░ ░░ ░     ░    ░░   ░     
+               ░        ░        ░      ░        ░ ░     ░        ░        ░ ░       ░  ░  ░░   ░   ░ ░     ░  ░   ░   ░     ░         
+
+"""
+
+def display_menu():
+    print(ART)
+    print("==================================")
+    print("    Password Vulnerability Checker ")
+    print("==================================")
+    print("Enter 'q' to quit.")
 
 def check_password(password, target_password):
     return password == target_password
-
 
 def load_rockyou_wordlist(file_path):
     with open(file_path, 'r', encoding='latin-1') as f:
         return [line.strip() for line in f]
 
-
 def brute_force_cracker(target_password, password_list):
     start_time = time.time()
-
     for password in password_list:
         if check_password(password, target_password):
             end_time = time.time()
-            print(f"Password is vulnerable! Found in wordlist.")
+            print(f"Password is vulnerable! Found in 'rockyou.txt' wordlist.")
             print(f"Time taken: {end_time - start_time:.2f} seconds")
             return
-
     end_time = time.time()
-    print("Password is not found in the wordlist. It's less likely to be vulnerable.")
+    print("Password is not found in the wordlist. It's less likely to be vulnerable to bruteforcing attacks.")
     print(f"Time taken: {end_time - start_time:.2f} seconds")
 
-
 if __name__ == "__main__":
-    # Take user input for the password to check
-    user_password = input("Enter the password you want to check: ")
+    display_menu()
 
-    # Load the RockYou wordlist
-    password_list = load_rockyou_wordlist("/usr/share/wordlists/rockyou.txt")
+    while True:
+        user_input = input("Enter the password you want to check (or 'q' to quit): ")
 
-    # Check if the user's password is in the RockYou wordlist
-    brute_force_cracker(user_password, password_list)
+        if user_input.lower() == 'q':
+            print("Goodbye!")
+            break
+
+        # Load the RockYou wordlist
+        password_list = load_rockyou_wordlist("/usr/share/wordlists/rockyou.txt")
+
+        # Check if the user's password is in the RockYou wordlist
+        brute_force_cracker(user_input, password_list)
